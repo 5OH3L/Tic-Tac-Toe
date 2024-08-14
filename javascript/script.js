60ff6ffd3174ef91856ed7d386c3975322278bc0
Sohel <152502004+5OH3L@users.noreply.github.com>
2024-08-14 17:55:38 +0530

    Tic Tac Toe Console Version
    This one doesn't have winning condition and players can place marker on a cell that is already filled.

---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+const Gameboard = function(){
+    const board = [[0,0,0],
+                   [0,0,0],
+                   [0,0,0]];
+    function getBoard(){return board};
+    return {getBoard};
+}();
+const GameController = function(){
+    let activePlayerMarker = 1;
+    const board = Gameboard.getBoard();
+    function placeMarker(row, column){
+        board[row][column] = activePlayerMarker;
+        switchPlayer();
+    }
+    function switchPlayer(){
+        activePlayerMarker = activePlayerMarker == 1 ? 2 : 1;
+    }
+    return {placeMarker}
+}();
+
+const Player = function(name, marker){
+    this.name = name;
+    this.marker = marker;
+    const getName = () => this.name;
+    const getMarker = () => this.marker;
+    return {getName,getMarker};
+};
+const playerOne = new Player("Player-One", 1);
+const playerTwo = new Player("Player-Two", 2);
+
+console.table(Gameboard.getBoard());
