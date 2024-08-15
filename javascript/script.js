4d6aec0c5c513a33b04fe88e3550f3e5fa320dc0
Sohel <152502004+5OH3L@users.noreply.github.com>
2024-08-15 22:09:02 +0530

    Tic Tac Toe Console Version

--- javascript/script.js
+++ javascript/script.js
@@ -1,33 +1,82 @@
-const Gameboard = function () {
-    const board = [[0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]];
-    function getBoard() { return board };
-    return { getBoard };
-}();
-const GameController = function () {
-    let activePlayerMarker = 1;
-    const board = Gameboard.getBoard();
-    function placeMarker(row, column) {
-        if (board[row][column] == 0) {
-            board[row][column] = activePlayerMarker;
-            switchPlayer();
-        }
-    }
-    function switchPlayer() {
-        activePlayerMarker = activePlayerMarker == 1 ? 2 : 1;
-    }
-    return { placeMarker }
-}();
+const Gameboard = (function () {
+    let row = 3;
+    let column = 3;
+    const board = [];
 
-const Player = function (name, marker) {
-    this.name = name;
-    this.marker = marker;
-    const getName = () => this.name;
-    const getMarker = () => this.marker;
-    return { getName, getMarker };
-};
-const playerOne = new Player("Player-One", 1);
-const playerTwo = new Player("Player-Two", 2);
+    function initBoard() {
+        board.length = 0;
+        for (let i = 0; i < row; i++) {
+            board[i] = [];
+            for (let j = 0; j < column; j++) {
+                board[i].push(0);
+            }
+        }
+    };
+    initBoard();
+
+    return {
+        getBoard: () => board,
+        resetBoard: () => initBoard(),
+        setSize: (newRow, newColumn) => {
+            row = newRow;
+            column = newColumn;
+            initBoard();
+        },
+    };
+})();
+
+const GameController = (function () {
+    const board = Gameboard.getBoard()
+    let activePlayerMarker = 1;
+
+    const switchPlayer = () => activePlayerMarker = activePlayerMarker === 1 ? 2 : 1;
+
+    function placeMarker(row, column) {
+        if (board[row][column] === 0) {
+            board[row][column] = activePlayerMarker;
+            if (checkWin()) {
+                alert(activePlayerMarker === 1 ? "Player One Won" : "Player Two Won");
+                Gameboard.resetBoard();
+            } else {
+                switchPlayer();
+            }
+            console.table(board);
+            console.log(activePlayerMarker === 1 ? "Player One's Turn" : "Player Two's Turn")
+        } else {
+            console.log(`The cell is already filled (${row},${column})`)
+        }
+    };
+
+    function checkWin() {
+        const size = board.length;
+
+        // Row Check
+        for (let i = 0; i < size; i++) {
+            if (board[i].every(cell => cell === activePlayerMarker)) {
+                return true;
+            }
+        }
+
+        // Column Check
+        for (let i = 0; i < size; i++) {
+            if (board.every(row => row[i] === activePlayerMarker)) {
+                return true;
+            }
+        }
+
+        // Diagonal Check Top-left To Bottom-right
+        if (board.every((row, index) => row[index] === activePlayerMarker)) {
+            return true;
+        }
+
+        // Diagonal Check Bottom-right to Top-left
+        if (board.every((row, index) => row[size - 1 - index] === activePlayerMarker)) {
+            return true;
+        }
+        return false;
+    };
+
+    return { placeMarker, checkWin }
+})();
 
 console.table(Gameboard.getBoard());
